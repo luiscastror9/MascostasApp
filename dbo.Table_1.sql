fd299fce8a906e721738c76355ea461dc23de11e
Belen <bnfkjs@live.com>
2017-08-25 21:27:28 -0300

    Version 1
    Primera version .net la cual incluye el proyecto en visual studio y dos bases de datos

---
+++ dbo.Table_1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Pub]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Raza] NVARCHAR(15) NULL, 
+    [Ubicacion] NVARCHAR(30) NOT NULL, 
+    [Sexo] NVARCHAR(10) NOT NULL, 
+    [Vacunas] NVARCHAR(MAX) NULL, 
+    [Edad] INT NULL, 
+    [Descripcion] TEXT NOT NULL, 
+    [Status] INT NULL
+)
---
+++ dbo.Table_2.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [nombre status] NVARCHAR(MAX) NULL
+)
