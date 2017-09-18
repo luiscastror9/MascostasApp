44c6e7b587c9e8a5aaaf834d2aad00eeb4c47eaf
Matias <tubiomatias@hotmail.com>
2017-09-18 02:19:28 -0300

    18.09

--- ProyectoMascotas/Models/ValidarUsuario.cs
+++ ProyectoMascotas/Models/ValidarUsuario.cs
@@ -10,24 +10,35 @@ namespace ProyectoMascotas.Models
     public class ValidarUsuario
     {
         public int ID { get; set; }
-        [Required(ErrorMessage = "La razon social es un dato obligatorio")]
+        
         public string Empresa { get; set; }
-        [Required(ErrorMessage = "El nombre es un dato obligatorio")]
+
+
         public string Nombre { get; set; }
-        [Required(ErrorMessage = "El apellido es un dato obligatorio")]
+
+
         public string Apellido { get; set; }
-        [Required(ErrorMessage = "El nombre de usuario es un dato obligatorio")]
+
+        [Required(ErrorMessage = "Este dato es obligatorio")]
         public string Nombre_de_Usuario { get; set; }
-        [Required(ErrorMessage = "La contraseña es un dato obligatorio")]
+
+        [Required(ErrorMessage = "Este dato es obligatorio")]
         public string Pass { get; set; }
-        [Required(ErrorMessage = "La localidad es un dato obligatorio")]
+
+        [Required(ErrorMessage = "Este dato es obligatorio")]
         public string Localidad { get; set; }
-        [Required(ErrorMessage = "La fecha es un dato obligatorio")]
+
+        [Display(Name = "fecha de nacimiento")]
+        [Required(ErrorMessage = "Este dato es obligatorio")]
         public Nullable<System.DateTime> Fecha_de_nacimiento { get; set; }
-        [Required(ErrorMessage = "El DNI/CUIT es un dato obligatorio")]
+
+        [Required(ErrorMessage = "Este dato es obligatorio")]
         public string DNI { get; set; }
+        
         public string Tipo_de_usuario { get; set; }
-        [Required(ErrorMessage = "El correo electronico es un dato obligatorio")]
+
+        [Required(ErrorMessage = "Este dato es obligatorio")]
+        [EmailAddress(ErrorMessage="Email invalido")]
         public string Email { get; set; }
     }
 }
