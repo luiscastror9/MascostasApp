e62b30d343cf816441023fdd6689bfd982924914
lFacu <facutadich@gmail.com>
2017-09-20 21:19:46 -0300

    clase20.09
    agregamos layaut inicio, con referencia a un login.

--- ProyectoMascotas/Controllers/HomeController.cs
+++ ProyectoMascotas/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace ProyectoMascotas.Controllers
 
             return View();
         }
+
     }
 }
---
+++ ProyectoMascotas/Controllers/PaginaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMascotas.Controllers
+{
+    public class PaginaController : Controller
+    {
+        //
+        // GET: /Pagina/
+        public ActionResult Pagina()
+        {
+            return View();
+        }
+	}
+}
